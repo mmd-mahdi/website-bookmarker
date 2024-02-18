3497e376fb5ca64930197e8427286839ad07ad37
Mohammad-Mahdi-Mostofian-Zadeh <mm.m.mahdi.4883@gmail.com>
2024-02-18 14:42:46 +0330

    Json file has been added

--- python/firstAi.py
+++ python/firstAi.py
@@ -1,24 +1,31 @@
 import webbrowser
 import clipboard
+import json
 
-websites = {"google":'https://www.google.com'}
+with open("data.json") as database:
+  temp = database.read()
+
+
+data = json.loads(temp)
 print("hi what website do you looking for?")
 inp = input()
-while len(inp) :
-  if inp in websites:
+while len(inp):
+  if inp in data:
     print("Great! I got the URL! Choose one of this two option "
           "1.Copy to clipborad"
           "2.Open website")
     index = int(input())
     if index == 1:
-      clipboard.copy(websites[inp])
+      clipboard.copy(data[inp])
+      print("test 1 checked")
     elif index == 2:
-      webbrowser.open(websites[inp], new=0, autoraise=True)
+      webbrowser.open(data[inp], new=0, autoraise=True)
     else:
       print("unknown input")
   else:
     print("i dont have this website pls add url")
-    websites[inp] = input()  
+    data[inp] = input()
+    # database.write(data[inp])
     print("thans for adding some info")
   inp = input()
 
